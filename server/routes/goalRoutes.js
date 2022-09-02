bce24745c45559ece26b40e34e10a2f2ba817043
BRlogical <bhavik@rlogical.com>
2022-09-02 14:40:01 +0530

    Update: Goals controller, route setup, error handler and middleware

--- server/routes/goalRoutes.js
+++ server/routes/goalRoutes.js
@@ -1,8 +1,16 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const {
+  getGoals,
+  updateGoal,
+  setGoal,
+  deleteGoal
+} = require("../controllers/goalController");
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Get goals" });
-});
+// @desc cleaner way to write goal if route is same for all request
+ 
+router.route("/").get(getGoals).post(setGoal);
+router.route("/:id").put(updateGoal).delete(deleteGoal);
 
-module.exports = router
+
+module.exports = router;
